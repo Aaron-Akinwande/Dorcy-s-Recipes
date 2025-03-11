81ac83ab2677aed17b5c65bead4e83ef5768e79b
Aaron <aaronakinwande@gmail.com>
2025-03-11 14:28:14 +0100

    first -v of header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
+import Header from '@/components/Header'
 import React from 'react'
 
 export default function index() {
   return (
-    <div>index</div>
+    <div className=' flex flex-col'>
+      
+        <Header />
+      
+      <div className=' h-48 bg-black'></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
   )
 }
